e1e3fac6d575fccf4b0e9f4fe833d299397b9e48
Flávio Irala <flavioiralap@gmail.com>
2025-05-19 18:52:49 -0400

    Primeiro commit

---
+++ src/styles.ts
@@ -0,0 +1,29 @@
+import styled, { createGlobalStyle } from 'styled-components'
+
+export const cores = {
+  branca: '#EEEEEE;',
+  preta: '#111;',
+  cinza: '#333;',
+  verde: '#10AC84'
+}
+
+export const GlobalCss = createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        font-family: Roboto, sans-serif;
+        list-style: none;
+    }
+    body {
+        background-color: ${cores.preta};
+        color: ${cores.branca};
+        padding-top: 40px 
+    }
+`
+
+export const Container = styled.div`
+  margin: 0 auto;
+  width: 100%;
+  max-width: 1024px;
+`
