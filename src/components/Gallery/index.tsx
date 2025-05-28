d67323725a428d1dd9c9cfe94b6ae236af4dc764
Flávio Irala <flavioiralap@gmail.com>
2025-05-27 22:45:28 -0400

    Dando continuidade ao curso

--- src/components/Gallery/index.tsx
+++ src/components/Gallery/index.tsx
@@ -1,6 +1,7 @@
 import Section from '../Section'
-import { Item, Items, Action } from './styles'
+import { Item, Items, Action, Modal, ModalContent } from './styles'
 
+import fechar from '../../assets/images/close.png'
 import hogwarts from '../../assets/images/fundo_hogwarts.png'
 import spiderman from '../../assets/images/banner-homem-aranha.png'
 import zoom from '../../assets/images/mais-zoom.png'
@@ -42,24 +43,36 @@ const Gallery = ({ defaultCover, name }: Props) => {
   }
 
   return (
-    <Section title="Galeria" background="black">
-      <Items>
-        {mock.map((media, index) => (
-          <Item key={media.url}>
-            <img
-              src={getMidiaCover(media)}
-              alt={`Mídia ${index + 1} de ${name}`}
-            />
-            <Action>
+    <>
+      <Section title="Galeria" background="black">
+        <Items>
+          {mock.map((media, index) => (
+            <Item key={media.url}>
               <img
-                src={getMidiaIcon(media)}
-                alt="Click para maximizar a mídia"
+                src={getMidiaCover(media)}
+                alt={`Mídia ${index + 1} de ${name}`}
               />
-            </Action>
-          </Item>
-        ))}
-      </Items>
-    </Section>
+              <Action>
+                <img
+                  src={getMidiaIcon(media)}
+                  alt="Click para maximizar a mídia"
+                />
+              </Action>
+            </Item>
+          ))}
+        </Items>
+      </Section>
+      <Modal>
+        <ModalContent className="container">
+          <header>
+            <h4>{name}</h4>
+            <img src={fechar} alt="Icone de fechar" />
+          </header>
+          <img src={spiderman} alt="" />
+        </ModalContent>
+        <div className="overlay"></div>
+      </Modal>
+    </>
   )
 }
 export default Gallery
